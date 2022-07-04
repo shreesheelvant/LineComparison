09ed328c5738c909e26e3c8cbcd307a634aad3f4
unknown <suntanabhagyashree@gmail.com>
2022-07-04 11:12:40 +0530

    ADDED:Calculate its Length

--- src/com/bridgelabz/CalculateLength.java
+++ src/com/bridgelabz/CalculateLength.java
@@ -1,7 +1,26 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class CalculateLength {
+    public static void calculateLengthUC1() {
+        System.out.println("Calculate Length between two points");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter x1 : ");
+        int x1 = scanner.nextInt();
+        System.out.println("Enter y1 : ");
+        int y1 = scanner.nextInt();
+        System.out.println("Enter x2 : ");
+        int x2 = scanner.nextInt();
+        System.out.println("Enter y2 : ");
+        int y2 = scanner.nextInt();
+        double length = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+        System.out.println(length);
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison");
+        calculateLengthUC1();
+
+
+
     }
 }
